c386c268a67f937313e890170f9ff281490d9408
oksglm <omersaglam88@gmail.com>
2019-11-23 12:56:13 +0300

    Code Refactoring

--- src/main/java/com/cafeintech/multaxi/share/servlets/GenerateToken.java
+++ src/main/java/com/cafeintech/multaxi/share/servlets/GenerateToken.java
@@ -23,19 +23,16 @@ import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
-import com.taxibus.remote.tools.TokenHelper;
+import com.cafeintech.multaxi.remote.tools.TokenHelper;
 
 // With @WebServlet annotation the webapp/WEB-INF/web.xml is no longer required.
 @WebServlet(name = "GenerateToken", value = "/token")
 public class GenerateToken extends HttpServlet {
-
-	
 	
 	private static final long serialVersionUID = -3022351001311893239L;
 
 	@Override
 	public void doGet(HttpServletRequest request, HttpServletResponse response) throws IOException {
-
 		response.setContentType("text/plain");
 		response.getWriter().println(TokenHelper.getInstance().getToken());
 	}
