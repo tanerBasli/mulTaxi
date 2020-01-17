b03ea47fde82ab377bfc5f90e8930b4472b3b119
Kerem Akkaya <keremakkaya@gmail.com>
2020-01-17 10:00:14 +0300

    P19-935 Coordinate calculations

--- src/main/java/com/cafeintech/multaxi/controller/quaternion/Quaternion.java
+++ src/main/java/com/cafeintech/multaxi/controller/quaternion/Quaternion.java
@@ -29,7 +29,7 @@
  * NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS
  * SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
  */
-package com.multaxi.controller.quaternion;
+package com.cafeintech.multaxi.controller.quaternion;
 
 import java.util.logging.Logger;
 
