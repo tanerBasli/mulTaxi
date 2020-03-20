d2766c1cb59d7712725df25f652e09dbd889ab5b
Kerem Akkaya <keremakkaya@gmail.com>
2020-03-20 17:01:53 +0300

    initialization finalized

--- src/main/java/com/cafeintech/multaxi/controller/quaternion/Vector3f.java
+++ src/main/java/com/cafeintech/multaxi/controller/quaternion/Vector3f.java
@@ -30,7 +30,7 @@
  * SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
  */
 
-package com.multaxi.controller.quaternion;
+package com.cafeintech.multaxi.controller.quaternion;
 
 import javax.annotation.Nonnull;
 
