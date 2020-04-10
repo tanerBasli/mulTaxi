15942fc6500cafbd77712cfeb1d31b78e133581b
Taner Başlı <tanerbasli87@gmail.com>
2020-04-10 12:54:44 +0300

    Models are created

---
+++ src/main/java/com/taxibus/client/model/GsmModel.java
@@ -0,0 +1,34 @@
+package com.taxibus.client.model;
+
+public class GsmModel extends BaseModel {
+
+	private String countryCode;
+	private String areaCode;
+	private String number;
+	private String fullNumber;
+	public String getCountryCode() {
+		return countryCode;
+	}
+	public void setCountryCode(String countryCode) {
+		this.countryCode = countryCode;
+	}
+	public String getAreaCode() {
+		return areaCode;
+	}
+	public void setAreaCode(String areaCode) {
+		this.areaCode = areaCode;
+	}
+	public String getNumber() {
+		return number;
+	}
+	public void setNumber(String number) {
+		this.number = number;
+	}
+	public String getFullNumber() {
+		return fullNumber;
+	}
+	public void setFullNumber(String fullNumber) {
+		this.fullNumber = fullNumber;
+	}
+	
+}
---
+++ src/main/java/com/taxibus/client/model/UserCoordinateModel.java
@@ -0,0 +1,21 @@
+package com.taxibus.client.model;
+
+public class UserCoordinateModel extends BaseModel {
+
+	private UserModel user;
+	private CoordinateModel coordinate;
+	public UserModel getUser() {
+		return user;
+	}
+	public void setUser(UserModel user) {
+		this.user = user;
+	}
+	public CoordinateModel getCoordinate() {
+		return coordinate;
+	}
+	public void setCoordinate(CoordinateModel coordinate) {
+		this.coordinate = coordinate;
+	}
+	
+	
+}
