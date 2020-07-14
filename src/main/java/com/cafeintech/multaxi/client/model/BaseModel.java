914204954049c30376c1c15cc57a99dd158b9c74
oksglm <omersaglam88@gmail.com>
2020-07-14 10:06:59 +0300

    Base Model Serializable

--- src/main/java/com/cafeintech/multaxi/client/model/BaseModel.java
+++ src/main/java/com/cafeintech/multaxi/client/model/BaseModel.java
@@ -1,9 +1,36 @@
 package com.cafeintech.multaxi.client.model;
 
-public class BaseModel {
+import java.io.Serializable;
 
+public class BaseModel implements Serializable {
+
+	private static final long serialVersionUID = -3463693460375367136L;
 	private String token;
 	private String username;
 	private String date;
-	
+
+	public String getToken() {
+		return token;
+	}
+
+	public void setToken(String token) {
+		this.token = token;
+	}
+
+	public String getUsername() {
+		return username;
+	}
+
+	public void setUsername(String username) {
+		this.username = username;
+	}
+
+	public String getDate() {
+		return date;
+	}
+
+	public void setDate(String date) {
+		this.date = date;
+	}
+
 }
