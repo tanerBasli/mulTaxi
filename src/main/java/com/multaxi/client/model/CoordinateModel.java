cc2c10594a959cb87b369ddd382acae90c286441
Taner Başlı <tanerbasli87@gmail.com>
2021-03-18 15:05:40 +0300

    Coordinate models

--- src/main/java/com/multaxi/client/model/CoordinateModel.java
+++ src/main/java/com/multaxi/client/model/CoordinateModel.java
@@ -4,18 +4,30 @@ public class CoordinateModel extends BaseModel {
 
 	private String longitude;
 	private String latitude;
+	private String maxDistance;
+
 	public String getLongitude() {
 		return longitude;
 	}
+
 	public void setLongitude(String longitude) {
 		this.longitude = longitude;
 	}
+
 	public String getLatitude() {
 		return latitude;
 	}
+
 	public void setLatitude(String latitude) {
 		this.latitude = latitude;
 	}
-	
-	
+
+	public String getMaxDistance() {
+		return maxDistance;
+	}
+
+	public void setMaxDistance(String maxDistance) {
+		this.maxDistance = maxDistance;
+	}
+
 }
