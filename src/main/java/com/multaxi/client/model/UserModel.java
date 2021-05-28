fa7719078f0e9a6fa392d779bbe9d52f9e648cea
Taner Başlı <tanerbasli87@gmail.com>
2021-05-28 10:42:36 +0300

    random numbers for token

--- src/main/java/com/multaxi/client/model/UserModel.java
+++ src/main/java/com/multaxi/client/model/UserModel.java
@@ -5,27 +5,43 @@ import java.util.List;
 public class UserModel extends BaseModel {
 
 	private String userId;
-	private String deviceId;
-	private List<GsmModel> gsm;
 	
+	private String deviceId;
+	
+	private List<GsmModel> gsm;
+
+	private List<String> settings;
+
 	public String getUserId() {
 		return userId;
 	}
+
 	public void setUserId(String userId) {
 		this.userId = userId;
 	}
+
 	public String getDeviceId() {
 		return deviceId;
 	}
+
 	public void setDeviceId(String deviceId) {
 		this.deviceId = deviceId;
 	}
+
 	public List<GsmModel> getGsm() {
 		return gsm;
 	}
+
 	public void setGsm(List<GsmModel> gsm) {
 		this.gsm = gsm;
 	}
-	
-	
+
+	public List<String> getSettings() {
+		return settings;
+	}
+
+	public void setSettings(List<String> settings) {
+		this.settings = settings;
+	}
+
 }
---
+++ src/main/java/com/multaxi/controller/util/RandomNumberGenerator.java
@@ -0,0 +1,46 @@
+package com.multaxi.controller.util;
+
+import java.util.Random;
+
+public class RandomNumberGenerator {
+	
+	
+	public static String usage = "Usage: java RandomNumberGenerator <lines> [seed]";
+
+	public static void main(String[] args) {
+		if (args.length > 2) {
+			System.err.println("Too many arguments!");
+			System.err.println(usage);
+			System.exit(1);
+		} else if (args.length == 0) {
+			System.err.println("Requires at least one argument");
+			System.err.println(usage);
+			System.exit(1);
+		}
+
+		int count = parseCriticalInt(args[0]);
+
+		Random rng = null;
+		if (args.length == 2) {
+			int seed = parseCriticalInt(args[1]);
+			rng = new Random(seed);
+		} else {
+			rng = new Random();
+		}
+
+		for (int i = 0; i < count; ++i) {
+			System.out.println(rng.nextInt(9));
+		}
+	}
+
+	public static int parseCriticalInt(String stringValue) {
+		int intValue = 0;
+		try {
+			intValue = Integer.parseInt(stringValue);
+		} catch (NumberFormatException e) {
+			System.err.println(stringValue + " must be an integer");
+			System.exit(2);
+		}
+		return intValue;
+	}
+};
